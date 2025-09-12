20f673a4ae8958257a3bf41acf5bd4a0c981ece8
James Chapman <chapmanjames@google.com>
2025-09-12 16:39:17 -0700

    resnet testing simplifications, absl, and testing optional dependencies

--- bonsai/models/resnet50/tests/test_outputs.py
+++ bonsai/models/resnet50/tests/test_outputs.py
@@ -15,53 +15,39 @@
 import os
 
 import jax
-import jax.numpy as jnp
+import numpy as np
+import torch
+from absl.testing import absltest
 from huggingface_hub import snapshot_download
+from transformers import ResNetForImageClassification
 
 from bonsai.models.resnet50 import modeling as model_lib
 from bonsai.models.resnet50 import params
 
 
-def full_test(MODEL_CP_PATH=None):
-    # 1. Try additional imports
-    try:
-        import torch
-        from transformers import ResNetForImageClassification
-    except ModuleNotFoundError as e:
-        print(f"Skipping model output verification: {e}")
-        return
+class Resnet50Test(absltest.TestCase):
+    def test_full(self):
+        model_name = "microsoft/resnet-50"
+        MODEL_CP_PATH = "/tmp/models-bonsai/resnet-50"
 
-    # 2. Download safetensors file
-    model_name = "microsoft/resnet-50"
-    if MODEL_CP_PATH is None:
-        MODEL_CP_PATH = "/tmp/models-bonsai/" + model_name.split("/")[1]
+        if not os.path.isdir(MODEL_CP_PATH):
+            snapshot_download(model_name, local_dir=MODEL_CP_PATH)
+        safetensors_path = os.path.join(MODEL_CP_PATH, "model.safetensors")
+        bonsai_model = params.create_resnet50_from_pretrained(safetensors_path)
+        baseline_model = ResNetForImageClassification.from_pretrained(model_name)
 
-    if not os.path.isdir(MODEL_CP_PATH):
-        snapshot_download(model_name, local_dir=MODEL_CP_PATH)
+        batch_size, image_size = 8, 224
+        random_inputs = jax.random.truncated_normal(
+            jax.random.key(0), lower=-1, upper=1, shape=(batch_size, image_size, image_size, 3)
+        )
+        baseline_inputs = {"pixel_values": torch.tensor(random_inputs).to(torch.float32).permute(0, 3, 1, 2)}
 
-    safetensors_path = os.path.join(MODEL_CP_PATH, "model.safetensors")
+        bonsai_outputs = model_lib.forward(bonsai_model, random_inputs)
+        with torch.no_grad():
+            baseline_outputs = baseline_model(**baseline_inputs).logits.cpu().detach().numpy()
 
-    # 3. Load pretrained models
-    bonsai_model = params.create_resnet50_from_pretrained(safetensors_path)
-    baseline_model = ResNetForImageClassification.from_pretrained(model_name)
-
-    # 4. Create inputs
-    batch_size, image_size = 8, 224
-    random_inputs = jax.random.truncated_normal(
-        jax.random.key(0), lower=-1, upper=1, shape=(batch_size, image_size, image_size, 3)
-    )
-    baseline_inputs = {"pixel_values": torch.tensor(random_inputs).to(torch.float32).permute(0, 3, 1, 2)}
-
-    # 5. Compute model outputs
-    bonsai_outputs = model_lib.forward(bonsai_model, random_inputs)
-    with torch.no_grad():
-        baseline_outputs = baseline_model(**baseline_inputs).logits.cpu().detach().numpy()
-
-    # 6. Compare model outputs
-    atol = jnp.max(jnp.abs(bonsai_outputs - baseline_outputs))
-    rtol = atol / jnp.max(jnp.abs(baseline_outputs))
-    print(f"ATOL = {atol}\tRTOL = {rtol}")
+        np.testing.assert_allclose(bonsai_outputs, baseline_outputs, rtol=1e-5)
 
 
 if __name__ == "__main__":
-    full_test()
+    absltest.main()
